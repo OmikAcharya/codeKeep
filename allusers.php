85590c084368f2c93ea78854fa7459c16f52c7a8
OmikAcharya <omik.acharya@somaiya.edu>
2025-04-19 23:58:11 +0530

    fix: OMG!

--- allusers.php
+++ allusers.php
@@ -313,7 +313,7 @@ if (isset($_SESSION['name']) && isset($_SESSION['email'])) {
         function escapeHtml(unsafe) {
             return unsafe
                 .replace(/&/g, "&amp;")
-                .replace(/<//g, "&lt;")
+                .replace(/</g, "&lt;")
                 .replace(/>/g, "&gt;")
                 .replace(/"/g, "&quot;")
                 .replace(/'/g, "&#039;");
